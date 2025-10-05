fa675bdd711a7de3400f04bf6fb2c5419e671974
Supradeep13112 <supraroydeep2002@gmail.com>
2025-10-06 01:50:01 +0530

    Sum program

---
+++ Sum.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Sum {
+    public static void main(String[] args) {
+        int a,b;
+        Scanner s=new Scanner(System.in);
+        System.out.print("Enter 1st number:");
+        a=s.nextInt();
+        System.out.print("Enter 2nd number");
+        b=s.nextInt();
+        System.out.println("Sum of "+a+" and "+b+" is "+(a+b));
+    }
+}
